555a5ed5b391ffd99a950aa157d48c2fea1acd74
Odei Alba <oalbaab14dw@ikzubirimanteo.com>
2016-10-11 07:10:16 +0000

    crear y leer funciona

--- crear.php
+++ crear.php
@@ -10,5 +10,20 @@
     $renderer->setWidth(256);
     $writer = new BaconQrCode\Writer($renderer);
     $writer->writeFile($texto, 'codes/'.$nombre.'.png');
+    
+    $fichero = 'codes/'.$nombre.'.png';
+
+    if (file_exists($fichero)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($fichero).'"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($fichero));
+        readfile($fichero);
+        unlink($fichero);
+        exit;
+    }
     header("location:index.html");
 ?>
--- leer.php
+++ leer.php
@@ -1,10 +1,20 @@
 <?php
     require __DIR__ . '/vendor/autoload.php';
     
-    $nombre = $_POST["nombre"];
-    
-    //Leer un codigo QR
-    $QRCodeReader = new Libern\QRCodeReader\QRCodeReader();
-    $qrcode_text = $QRCodeReader->decode("codes/".$nombre.".png");
-    echo $qrcode_text;
+    $nombre = $_FILES['nombre'];
+    if(!empty($_FILES['nombre']['name'])){
+        $temp = explode(".", $_FILES["nombre"]["name"]);
+        $newfilename = round(microtime(true)) . '.' . end($temp);
+        move_uploaded_file($_FILES["nombre"]["tmp_name"], "codes/" . $newfilename);
+        $code = $newfilename;
+        
+        //Leer un codigo QR
+        $QRCodeReader = new Libern\QRCodeReader\QRCodeReader();
+        $qrcode_text = $QRCodeReader->decode("codes/".$code);
+        echo $qrcode_text;
+        unlink("codes/".$code);
+        echo "<br/><a href='index.html'>Volver</a>";
+    } else {
+        header("location:index.html");
+    }
 ?>
