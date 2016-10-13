448cace017cb252b099bb035313761cb54a2cd36
Odei Alba <oalbaab14dw@ikzubirimanteo.com>
2016-10-13 09:15:57 +0000

    intentando email

--- crear.php
+++ crear.php
@@ -1,8 +1,13 @@
 <?php
     require __DIR__ . '/vendor/autoload.php';
     
+    //require 'PHPMailerAutoload.php';
+    //require("class.phpmailer.php");
+    
     $nombre = $_POST["nombre"];
     $texto = $_POST["texto"];
+    //$email = $_POST["email"];
+    //$pass = $_POST["pass"];
     
     //Crear un codigo QR
     $renderer = new BaconQrCode\Renderer\Image\Png();
@@ -33,6 +38,36 @@
             header('Accept-Ranges: bytes');
             readfile($fichero);
             unlink($fichero);
-        }
+        } /*else if ($_POST['action'] == 'Email') {
+            $mail = new PHPMailer;
+            //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'smtp.mail.yahoo.com';  // Specify main and backup SMTP servers
+            $mail->SMTPAuth = false;                               // Enable SMTP authentication
+            $mail->Username = 'odeialba@yahoo.es';                 // SMTP username
+            $mail->Password = $pass;                           // SMTP password
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 587;                                    // TCP port to connect to
+            $mail->setFrom('odeialba@yahoo.es', 'Odei Alba');
+            //$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+            $mail->addAddress($email);               // Name is optional
+            //$mail->addReplyTo('info@example.com', 'Information');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
+            $mail->addAttachment($fichero);         // Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Código QR';
+            $mail->Body    = 'El código QR está adjunto';
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            if(!$mail->send()) {
+                echo 'Message could not be sent.';
+                echo 'Mailer Error: ' . $mail->ErrorInfo;
+                unlink($fichero);
+            } else {
+                echo 'Message has been sent';
+                unlink($fichero);
+            }
+        }*/
     }
 ?>
